bfd8302c9c2264f48f9193b462bf383f68617391
Aditya Thakur <aditya1999thakur@gmail.com>
2020-06-28 20:29:12 +0530

    info page done

--- lib/screens/MapPage.dart
+++ lib/screens/MapPage.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:rrr/constants/info.dart';
@@ -14,43 +16,51 @@ class _MapPageState extends State<MapPage> {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.only(top: 30.0, left: 15.0, right: 15.0),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.max,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16.0),
-                child: Text(
-                  "Learn how\nyou can contribute",
-                  style: TextStyle(
-                      fontFamily:
-                          GoogleFonts.sourceSansPro(fontWeight: FontWeight.bold)
-                              .fontFamily,
-                      color: Colors.black,
-                      fontSize: 32),
-                ),
+        child: Stack(
+          children: <Widget>[
+            Container(
+              height: MediaQuery.of(context).size.height,
+              child: Align(alignment: Alignment.bottomRight,child: Image.asset("assets/images/plantbg.png")),
+            ),
+            SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16.0),
+                    child: Text(
+                      "Learn how\nyou can contribute",
+                      style: TextStyle(
+                          fontFamily:
+                              GoogleFonts.sourceSansPro(fontWeight: FontWeight.bold)
+                                  .fontFamily,
+                          color: Colors.black,
+                          fontSize: 32),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 16.0),
+                    child: Text(
+                      "Tap on each of these cards to learn more.",
+                      style: TextStyle(
+                          fontFamily:
+                              GoogleFonts.sourceSansPro(fontWeight: FontWeight.bold)
+                                  .fontFamily,
+                          color: Colors.black45,
+                          fontSize: 18),
+                    ),
+                  ),
+                  InfoCard(color: "FFCCCC", title: "Reuse", text: reuseText,),
+                  InfoCard(color: "FAE1B3", title: "Reduce", text: reduceText,),
+                  InfoCard(color: "D9F2D4", title: "Recycle", text: recycleText,),
+                  Container(
+                    height: 300,
+                  )
+                ],
               ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 16.0),
-                child: Text(
-                  "Tap on each of these cards to learn more.",
-                  style: TextStyle(
-                      fontFamily:
-                          GoogleFonts.sourceSansPro(fontWeight: FontWeight.bold)
-                              .fontFamily,
-                      color: Colors.black45,
-                      fontSize: 18),
-                ),
-              ),
-              InfoCard(color: Color(0xFFFFCCCC), title: "Reuse", text: reuseText,),
-              InfoCard(color: Color(0xFFFAE1B3), title: "Reduce", text: reduceText,),
-              InfoCard(color: Color(0xFFD9F2D4), title: "Recycle", text: recycleText,),
-              Container(
-                height: 30,
-              )
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
